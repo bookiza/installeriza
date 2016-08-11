5b7ce0cf5866d57a5f50776f9aa044c51dc844e6
Marvin Danig <marvin@marvindanig.com>
2016-08-11 14:04:10 -0400

    Fixing execspawn child process functionality

--- installer.js
+++ installer.js
@@ -1,9 +1,13 @@
 const execspawn = require('npm-execspawn');
 
 module.exports = {
-
     install() {
-        execspawn('npm i -S --silent');
-    }
 
+        const child = execspawn('npm i -S --silent');
+        
+        child.stderr.pipe(process.stderr);
+        child.stdout.pipe(process.stdout);
+
+        
+    }
 };
