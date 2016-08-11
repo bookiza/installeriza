ae0c788128b605f556c8bad506f8961181b2a176
Marvin Danig <marvin@marvindanig.com>
2016-08-11 14:20:22 -0400

    Strong mode, installer module encapsulated

--- installer.js
+++ installer.js
@@ -1,13 +1,12 @@
-const execspawn = require('npm-execspawn');
-
 module.exports = {
     install() {
+        'use strong';
 
+        const execspawn = require('npm-execspawn');
         const child = execspawn('npm i -S --silent');
-        
+
         child.stderr.pipe(process.stderr);
         child.stdout.pipe(process.stdout);
 
-        
     }
 };
